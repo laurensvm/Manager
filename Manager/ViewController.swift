69f2096e4c929b754966bea2c1b8d49ad04a1114
Laurens <laurens@vanmieghem.org>
2019-07-22 09:15:26 +1000

    Initial Commit

---
+++ Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Manager
+//
+//  Created by Laurens Van Mieghem on 22/07/2019.
+//  Copyright © 2019 Laurens Van Mieghem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
